87985bb629221d817c640bc607e2c7994c329a07
Jack <jfreier@macalester.edu>
2018-12-01 14:48:53 -0600

    Update GUI.py
    Added import capabilities

--- GUI.py
+++ GUI.py
@@ -10,13 +10,13 @@
 
 from tkinter import *
 import tkinter.messagebox as mBox
-import random
+import easygui
 
 # -------------------------------------------------------------------------------------------------
 # Global Variables:
 
 rootWin = None
-parsedTxt = None
+parsedText = None
 
 # -------------------------------------------------------------------------------------------------
 # GUI Main Function Definition:
@@ -26,46 +26,47 @@ def GUIMain():
     elements of it. Has no parameters and returns nothing."""
 
     global rootWin
-    global parsedTxt
+    global parsedText
 
     rootWin = Tk()
     rootWin.title("Syllabus Information Parser")
-    rootWin.configure(bg="#87CEEB")
+    rootWin.configure(bg="#CDC9C9")
 
+    parsedText = StringVar()
 
-    titleLabel = Label(rootWin, text="Syllabus Simplifier", bg="#9FB6CD", fg="gray1", padx=40, pady=5,
-                    font="Verdana 12 bold")
+    titleLabel = Label(rootWin, text="Syllabus Simplifier", bg="#CDC9C9", fg="gray1", padx=40, pady=5,
+                    font="Verdana 18 bold")
     titleLabel.grid(row=0, column=0)
 
-    label1 = Label(rootWin, text="Import your Syllabus:", bg="#87CEEB", fg="gray1", padx=5, pady=5,
+    label1 = Label(rootWin, text="Import your Syllabus:", bg="#CDC9C9", fg="gray1", padx=5, pady=5,
                     font="Verdana 12 bold italic")
     label1.grid(row=1, column=0)
 
-    label2 = Label(rootWin, text="Parse your Syllabus:", bg="#87CEEB", fg="gray1", padx=5, pady=5,
+    label2 = Label(rootWin, text="Parse your Syllabus:", bg="#CDC9C9", fg="gray1", padx=5, pady=5,
                     font="Verdana 12 bold italic")
     label2.grid(row=3, column=0)
 
-    label3 = Label(rootWin, text="Parsed Text:", bg="#87CEEB", fg="gray1", padx=5, pady=5,
+    label3 = Label(rootWin, text="Parsed Text:", bg="#CDC9C9", fg="gray1", padx=5, pady=5,
                     font="Verdana 12 bold italic")
     label3.grid(row=5, column=0)
 
-    importButton = Button(rootWin, text="Import", width=7, bg="#A2B5CD", fg="black", activeforeground="#2F4F4F", bd=3,
+    importButton = Button(rootWin, text="Import", width=7, bg="#F5F5F5", fg="black", activeforeground="#2F4F4F", bd=3,
                     font="Verdana 12 bold", relief=RIDGE, overrelief=SUNKEN, command=importFile)
     importButton.grid(row=2, column=0)
 
-    parseButton = Button(rootWin, text="Parse", width=7, bg="#A2B5CD", fg="black", activeforeground="#2F4F4F", bd=3,
+    parseButton = Button(rootWin, text="Parse", width=7, bg="#F5F5F5", fg="black", activeforeground="#2F4F4F", bd=3,
                     font="Verdana 12 bold", relief=RIDGE, overrelief=SUNKEN, command=parseSyllabus)
     parseButton.grid(row=4, column=0)
 
-    quitButton = Button(rootWin, text="Quit", width=7, bg="#A2B5CD", fg="#CD0000", activeforeground="#FF0000", bd=3,
+    quitButton = Button(rootWin, text="Quit", width=7, bg="#F5F5F5", fg="#CD0000", activeforeground="#FF0000", bd=3,
                     font="Verdana 12 bold", relief=RIDGE, overrelief=SUNKEN, command=quit)
     quitButton.grid(row=8, column=0)
 
-    txtEntry = Entry(rootWin, textvariable=parsedTxt, bg="#efefef", fg="black", bd=5, font="Verdana 12",
+    txtEntry = Entry(rootWin, textvariable=parsedText, bg="#efefef", fg="black", bd=5, font="Verdana 12",
                      justify=CENTER, relief=SUNKEN, width=20)
     txtEntry.grid(row=6, column=0)
 
-    label4 = Label(rootWin, text="", bg="#87CEEB", fg="gray1", padx=5, pady=0,
+    label4 = Label(rootWin, text="", bg="#CDC9C9", fg="gray1", padx=5, pady=0,
                     font="Verdana 12 bold italic")
     label4.grid(row=7, column=0)
 
@@ -76,11 +77,12 @@ def GUIMain():
 
 def importF():
     """Imports a .pdf file from the user's device."""
-    #...
+    filePath = easygui.fileopenbox()
+    return filePath
+
 
 def parseS():
     """Parses the syllabus to extract relevant information. Simplifies the .pdf file to contain relevant information."""
-    #...
 
 
 # -------------------------------------------------------------------------------------------------
@@ -95,6 +97,7 @@ def parseSyllabus():
     """Takes no inputs and returns nothing. Parses the previously imported syllabus using a helper function."""
     global rootWin
     parseS()
+    parsedText.set("ass")
 
 def quit():
     """Takes no inputs and returns nothing. Upon being called, destroys the GUI."""
